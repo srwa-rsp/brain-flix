d1dac9f83fa3e4aa29f4accc9520472840c5c9ef
srwa-rsp <srwa.rashidpoor.1993@gmail.com>
2024-07-11 15:17:56 -0400

    feat: add header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 
 import './App.css'
+import Header from './components/Header'
+import VideoPlayer from './components/VideoPlayer'
+import VideoDetails from './components/VideoDetails'
+import videos from './Data/video-details.json'
 
 function App() {
-
+  const videosList = videos
   return (
     <>
-
+    <Header />
+    <VideoPlayer video={videosList[0].video} />
+    <VideoDetails />
     </>
   )
 }
