f7fc3657337d7b9d4dcf854c96a1aa9452bac498
srwa-rsp <srwa.rashidpoor.1993@gmail.com>
2024-07-12 20:28:05 -0400

    refactor: Move main components from App to Main component

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,15 @@
 import { useState } from "react";
 import "./App.css";
 import Header from "./components/Header/Header";
-import VideoPlayer from "./components/VideoPlayer/VideoPlayer";
-import VideoDetails from "./components/VideoDetails/VideoDetails";
-import videos from "./Data/video-details.json";
-import NextVideos from "./components/NextVideos/NextVideos";
+import Main from "./components/Main/Main";
 
 function App() {
-  const videosList = videos;
 
-  const [selectedVideo, setSelectedVideo] = useState(videos[0]);
-  const [nextVideoList, setNextVideoList] = useState(videosList.slice(1));
-
-  const handelSelectVideo = (id) => {
-    const selected = videosList.filter((video) => video.id === id);
-    console.log(selected[0])
-    setSelectedVideo(selected[0]);
-    const notSelected = videosList.filter((video) => video.id !== id);
-    setNextVideoList(notSelected)
-  };
 
   return (
     <>
       <Header />
-      <VideoPlayer video={selectedVideo.video} />
-      <VideoDetails video={selectedVideo} />
-      <NextVideos
-        videos={nextVideoList}
-        handelSelectVideo={handelSelectVideo}
-      />
+      <Main />
     </>
   );
 }
