077d35edd9dc491981617db054560f2b618b1ab9
srwa-rsp <srwa.rashidpoor.1993@gmail.com>
2024-07-13 18:49:58 -0400

    style: removing comments and white spaces

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -5,6 +5,7 @@ import VideoDetails from "../VideoDetails/VideoDetails";
 import videosList from "../../Data/video-details.json";
 import NextVideos from "../NextVideos/NextVideos";
 import './Main.css'
+
 const Main = () => {
   const [selectedVideo, setSelectedVideo] = useState(videosList[0]);
   const [nextVideoList, setNextVideoList] = useState(videosList.slice(1));
