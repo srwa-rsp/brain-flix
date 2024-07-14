832dd2a097696779870e7026d28fc5ccb4758e87
srwa-rsp <srwa.rashidpoor.1993@gmail.com>
2024-07-14 11:51:31 -0400

    feat: add video controlers

--- src/components/VideoPlayer/VideoPlayer.jsx
+++ src/components/VideoPlayer/VideoPlayer.jsx
@@ -1,12 +1,34 @@
-import React from 'react'
-import './VideoPlayer.css'
+import React from "react";
+import "./VideoPlayer.css";
+import playIcon from "../../assets/Icons/play.svg";
+import volumeIcon from "../../assets/Icons/volume_up.svg";
+import fullscreenIcon from "../../assets/Icons/fullscreen.svg";
 
-const VideoPlayer = ({video}) => {
+const VideoPlayer = ({ video }) => {
   return (
-    <div className='video-container'>
-        <video  controls poster={video.image} className='video-container__player'></video>
-    </div>
-  )
-}
+    <figure className="video-container">
+      <video poster={video.image} className="video-container__player"></video>
+      <div className="controls">
+        <button type="button" className="controls__play controls__button">
+          <img src={playIcon} alt="play" />
+        </button>
+        <div className="controls__progress-container">
+          <progress value="0" min="0" className="controls__progress">
+            <div className="controls__scrubber"></div>
+          </progress>
+          <span className="controls__time">22:14 / 10:11</span>
+        </div>
+        <div className="controls__buttons">
+          <button type="button" className="controls__button controls__button--volume">
+            <img src={volumeIcon} alt="volume" />
+          </button>
+          <button type="button" className="controls__button controls__button--fullscreen">
+            <img src={fullscreenIcon} alt="fullscreen" />
+          </button>
+        </div>
+      </div>
+    </figure>
+  );
+};
 
-export default VideoPlayer
+export default VideoPlayer;
