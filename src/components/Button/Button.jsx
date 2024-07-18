7743e74566c17955a2ed3f991d5a8c66059b4f33
srwa-rsp <srwa.rashidpoor.1993@gmail.com>
2024-07-18 17:30:03 -0400

    feat: add color prop

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import './Button.css'
 
-const Button = ({title, icon}) => {
+const Button = ({title, icon, color}) => {
   return (
-    <button className='button'>
+    <button className={`button ${color === 'secondary'? `button__secondary` : ''}`}>
        {icon && <img src={icon} alt="icon" className="button__icon" />}
         {title}
     </button>
