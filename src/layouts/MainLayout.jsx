a66aee2c7464de6e50fc74c97e4303e683549948
srwa-rsp <srwa.rashidpoor.1993@gmail.com>
2024-07-19 16:14:23 -0400

    feat: add toastify to main layout

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,13 +1,23 @@
 import Header from '../components/Header/Header';
 import { Outlet } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
-export const MainLayout = ({ children }) => {
+export const MainLayout = () => {
   return (
     <div >
       <Header></Header>
       <main>
       <Outlet />
       </main>
+      <ToastContainer
+            position="top-center"
+            autoClose={2000}
+            closeOnClick
+            theme="light"
+            hideProgressBar={false}
+            bodyClassName="toast"
+          />
     </div>
   );
 };
