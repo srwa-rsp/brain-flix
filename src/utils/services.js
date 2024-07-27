7c1cc425723bc5ca60c5a08123b0856f8576917c
srwa-rsp <srwa.rashidpoor.1993@gmail.com>
2024-07-27 11:28:56 -0400

    feat: add new services

--- src/utils/services.js
+++ src/utils/services.js
@@ -1,10 +1,8 @@
 import axiosInstance from './axiosConfig';
 
-const api_key = '370bac3f-411a-4630-8df1-3f247b91009c'
-
 export const useGetVideos = async () => {
     try {
-        const response = await axiosInstance.get(`/videos?api_key=${api_key}`)
+        const response = await axiosInstance.get(`/videos`)
         return response.data
     } catch (error) {
         console.error('GetVideos Error:', error);
@@ -13,7 +11,7 @@ export const useGetVideos = async () => {
 
 export const useGetVideoById = async (id) => {
     try {
-        const response = await axiosInstance.get(`/videos/${id}?api_key=${api_key}`)
+        const response = await axiosInstance.get(`/videos/${id}`)
         return response.data
     } catch (error) {
         console.error('GetVideoById Error:', error);
@@ -22,9 +20,17 @@ export const useGetVideoById = async (id) => {
 
 export const usePostComment = async (id, comment) => {
     try {
-        const response = await axiosInstance.post(`/videos/${id}/comments?api_key=${api_key}`,comment)
+        const response = await axiosInstance.post(`/videos/${id}/comments`,comment)
         return response.data
     } catch (error) {
         console.error('PostComment Error:', error);
+    }
+}
+export const usePostVideo = async (video) => {
+    try {
+        const response = await axiosInstance.post(`/videos/`,video)
+        return response.data
+    } catch (error) {
+        console.error('PostVideo Error:', error);
     }
 }
